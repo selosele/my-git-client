d2c553fefee1a8f9790edd10ab4b9ea202dac4a6
selosele <osh3384@naver.com>
2024-01-20 18:53:42 +0900

    뷰와 뷰모델 소스 분리

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,117 @@
-﻿namespace MyGitClient.ViewModels;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
+using ReactiveUI;
+
+namespace MyGitClient.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    #region Properties
+    private string _repositoryPath = "Load Git repository...";
+    public string RepositoryPath
+    {
+        get => _repositoryPath;
+        set => this.RaiseAndSetIfChanged(ref _repositoryPath, value);
+    }
+
+    private string? _currentBranchText;
+    public string? CurrentBranchText {
+        get => _currentBranchText;
+        set => this.RaiseAndSetIfChanged(ref _currentBranchText, value);
+    }
+
+    private string? _latestCommitText;
+    public string? LatestCommitText {
+        get => _latestCommitText;
+        set => this.RaiseAndSetIfChanged(ref _latestCommitText, value);
+    }
+
+    private string? _latestCommitDateText;
+    public string? LatestCommitDateText {
+        get => _latestCommitDateText;
+        set => this.RaiseAndSetIfChanged(ref _latestCommitDateText, value);
+    }
+
+    private bool? _actionButtonsBorderVisible = false;
+    public bool? ActionButtonsBorderVisible {
+        get => _actionButtonsBorderVisible;
+        set => this.RaiseAndSetIfChanged(ref _actionButtonsBorderVisible, value);
+    }
+
+    /** <summary>커밋 정보 목록</summary> */
+    public ObservableCollection<CommitInfo>? Commits { get; set; }
+    #endregion
+
+    /** <summary>Git 저장소 정보를 가져와 화면에 표시한다.</summary> */
+    public async void UpdateRepoInfo(string repositoryPath)
+    {
+        try
+        {
+            if (string.IsNullOrWhiteSpace(repositoryPath) || !Directory.Exists(repositoryPath))
+            {
+                // 경로가 유효하지 않을 경우 사용자에게 메시지를 표출한다.
+                await AlertBox("Invalid repository path");
+                return;
+            }
+
+            using var repo = new Repository(repositoryPath);
+            Branch currentBranch = repo.Head;
+            Commit latestCommit = currentBranch.Tip;
+
+            var commits = new List<CommitInfo>();
+            foreach (Commit commit in repo.Commits)
+            {
+                commits.Add(
+                    new CommitInfo(
+                        commit.Sha,
+                        commit.Author.Name,
+                        commit.Author.Email,
+                        commit.Author.When.ToString(),
+                        commit.Message
+                    )
+                );
+            }
+            Commits = new ObservableCollection<CommitInfo>(commits);
+
+            RepositoryPath = repositoryPath;
+            CurrentBranchText = $"Current Branch: {currentBranch.FriendlyName}";
+            LatestCommitText = $"Latest Commit: {latestCommit.Message}";
+            LatestCommitDateText = $"Latest Commit Date: {latestCommit.Author.When}";
+            ActionButtonsBorderVisible = true;
+        }
+        catch (Exception ex)
+        {
+            await AlertBox(ex.Message);
+        }
+    }
+
+    /** <summary>사용자에게 알림 메시지를 표출한다.</summary> */
+    public static async Task<ButtonResult> AlertBox(string message, string title = "Alert")
+    {
+        var box = MessageBoxManager.GetMessageBoxStandard(title, message);
+        return await box.ShowAsync();
+    }
+
+    /** <summary>사용자에게 확인 메시지를 표출한다.</summary> */
+    public static async Task<ButtonResult> ConfirmBox(string message, string title = "Confirm")
+    {
+        var box = MessageBoxManager.GetMessageBoxStandard(title, message, ButtonEnum.YesNoCancel);
+        return await box.ShowAsync();
+    }
+}
+
+/** <summary>커밋 정보</summary> */
+public class CommitInfo(string sha, string authorName, string authorEmail, string authorWhen, string message)
+{
+    public string Sha { get; set; } = sha;
+    public string AuthorName { get; set; } = authorName;
+    public string AuthorEmail { get; set; } = authorEmail;
+    public string AuthorWhen { get; set; } = authorWhen;
+    public string Message { get; set; } = message;
 }
