2f8973eece1b5254695e032a6f88b5328e7dbb5d
selosele <osh3384@naver.com>
2024-02-02 11:36:49 +0900

    Git 사용자 정보 Model 클래스 분리

---
+++ Models/AuthorInfo.cs
@@ -0,0 +1,9 @@
+namespace MyGitClient.Models
+{
+    // <summary>Git 사용자 정보</summary>
+    public class AuthorInfo(string userName, string userEmail)
+    {
+        public string UserName { get; set; } = userName;
+        public string UserEmail { get; set; } = userEmail;
+    }
+}
